e3b0dc53836714f3b9f19825f13cd56cf7fc8e6d
Андрей Арямов <inikesb@mail.ru>
2022-10-23 22:30:31 +0300

    main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def start_training(char_name: str, char_class: str) -> str:
           '— чтобы блокировать атаку противника или special — чтобы'
           'использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -60,8 +60,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice: str = None
-    char_class: str = None
+    approve_choice = None
+    char_class = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь'
                            'играть: Воитель — warrior, Маг — mage,'
@@ -78,7 +78,7 @@ def choice_char_class() -> str:
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор,'
                                'или любую другую кнопку, чтобы выбрать'
                                'другого персонажа ').lower()
-    return char_class
+    return char_class if char_class is not None else ("char_class")
 
 
 def main():
